8b52b39056295c0b1d0167740262428865478592
Jon-Erik <jdc151@case.edu>
2018-06-30 14:17:54 -0400

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(process.cwd() + "/client/build"));
 }
 
 app.use(routes);
